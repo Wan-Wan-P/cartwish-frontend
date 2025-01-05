54a4931f6ef1b7fc8e407a08bff6150da22b09fe
Wan-Wan-P <52208007+Wan-Wan-P@users.noreply.github.com>
2025-01-05 10:24:08 -0500

    Finish developing all features - css defect to do

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -11,14 +11,14 @@ const HomePage = () => {
       <HeroSection
         title="Buy iPhone 14 Pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever"
-        link="/"
+        link="/product/67741f05db2306a1286de3e8"
         image={iphone}
       />
       <FeaturedProducts />
       <HeroSection
         title="Build the ultimate setup"
         subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini"
-        link="/"
+        link="/product/67741f05db2306a1286de3f0"
         image={mac}
       />
     </div>
--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -9,24 +9,28 @@ import Pagination from "../Common/Pagination";
 
 const ProductsList = () => {
   const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState("");
+  const [sortedProducts, setSortedProducts] = useState([]);
   const [search, setSearch] = useSearchParams();
   const category = search.get("category");
+  const searchQuery = search.get("search");
 
   const { data, error, isLoading } = useData(
     "/products",
     {
       params: {
+        search: searchQuery,
         category,
         perPage: 10,
         page,
       },
     },
-    [category, page]
+    [searchQuery, category, page]
   );
 
   useEffect(() => {
     setPage(1);
-  }, [category]);
+  }, [searchQuery, category]);
 
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -54,11 +58,38 @@ const ProductsList = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [data, isLoading]);
 
+  useEffect(() => {
+    if (data && data.products) {
+      const products = [...data.products];
+
+      if (sortBy === "price desc") {
+        setSortedProducts(products.sort((a, b) => b.price - a.price));
+      } else if (sortBy === "price asc") {
+        setSortedProducts(products.sort((a, b) => a.price - b.price));
+      } else if (sortBy === "rate desc") {
+        setSortedProducts(
+          products.sort((a, b) => b.reviews.rate - a.reviews.rate)
+        );
+      } else if (sortBy === "rate asc") {
+        setSortedProducts(
+          products.sort((a, b) => a.reviews.rate - b.reviews.rate)
+        );
+      } else {
+        setSortedProducts(products);
+      }
+    }
+  }, [sortBy, data]);
+
   return (
     <section className="product_list_section">
       <header className="align_center product_list_header">
         <h2>Products</h2>
-        <select name="sort" id="" className="products_sorting">
+        <select
+          name="sort"
+          id=""
+          className="products_sorting"
+          onChange={(e) => setSortBy(e.target.value)}
+        >
           <option value="">Relevance</option>
           <option value="price desc">Price HIGH to LOW</option>
           <option value="price asc">Price LOW to HIGH</option>
@@ -69,7 +100,7 @@ const ProductsList = () => {
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
         {data?.products &&
-          data.products.map((product) => (
+          sortedProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         {isLoading && skeleton.map((n) => <ProductCardSkeleton key={n} />)}
